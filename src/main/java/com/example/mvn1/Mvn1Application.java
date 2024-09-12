9e35a6078d42191af7ea54ba88695b100665ef31
adenali20 <adenali362@gmail.com>
2024-09-12 12:54:16 -0500

    first commit

---
+++ src/main/java/com/example/mvn1/Mvn1Application.java
@@ -0,0 +1,13 @@
+package com.example.mvn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvn1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvn1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvn1/Mvn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvn1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvn1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
