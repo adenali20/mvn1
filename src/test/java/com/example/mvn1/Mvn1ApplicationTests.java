0ad4e7891ce3ce429d6834f2e2795c83133b18f3
adenali20 <adenali362@gmail.com>
2024-09-16 15:20:01 -0500

    add test

--- src/test/java/com/example/mvn1/Mvn1ApplicationTests.java
+++ src/test/java/com/example/mvn1/Mvn1ApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.mvn1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class Mvn1ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void whenEmptyString_thenAccept() {
+//        Palindrome palindromeTester = new Palindrome();
+        assertTrue(1==1);
     }
 
 }
