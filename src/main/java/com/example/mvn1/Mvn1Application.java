ab0de72033c1cc1a82dac3d6f33c801b545fa7b0
adenali20 <adenali362@gmail.com>
2024-09-17 18:09:04 -0500

    Add users api

--- src/main/java/com/example/mvn1/Mvn1Application.java
+++ src/main/java/com/example/mvn1/Mvn1Application.java
@@ -2,6 +2,9 @@ package com.example.mvn1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Mvn1Application {
@@ -10,5 +13,14 @@ public class Mvn1Application {
         SpringApplication.run(Mvn1Application.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
--- src/main/java/com/example/mvn1/controller/TestController.java
+++ src/main/java/com/example/mvn1/controller/TestController.java
@@ -2,23 +2,37 @@ package com.example.mvn1.controller;
 
 import com.example.mvn1.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TestController {
     @Autowired
     TestService testService;
-    @GetMapping("/test/{str}")
 
+    @GetMapping("/test/{str}")
     public Map<String,Boolean> isPalindrome(@PathVariable String str) {
 
         Map<String,Boolean> res=new HashMap<>();
         res.put(str,testService.isPalindrome(str));
         return res;
+    }
+
+    @GetMapping("/users")
+    public List<String> isPalindrome() {
+
+       List<String> list=new ArrayList<>();
+       list.add("aden");
+       list.add("mohamed");
+       list.add("ali");
+        return list;
     }
 }
